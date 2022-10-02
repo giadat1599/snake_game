7cc9a949359c3ab34c5dc2389a5861eff05ed23b
giadat1599 <truonggiadat15@gmail.com>
2022-10-02 17:07:27 +0700

    feat: collision with wall and food

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 game_is_on = True
@@ -12,15 +14,32 @@ screen.tracer(0)
 screen.listen()
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.onkey(key="Up", fun=snake.up)
 screen.onkey(key="Down", fun=snake.down)
 screen.onkey(key="Left", fun=snake.left)
 screen.onkey(key="Right", fun=snake.right)
 
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.snake_head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.snake_head.xcor() > 280 or snake.snake_head.xcor() < -280 or snake.snake_head.ycor() > 280 or snake.snake_head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.snake_head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
